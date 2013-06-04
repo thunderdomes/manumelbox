e149445b0aa9ef1f79a752b688e2f2a81c012a9a
Arie <arie@wego.com>
2013-06-04 12:07:24 +0700

    initial commit

---
+++ Erdioo/Erdioo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Erdioo
+//
+//  Created by Arie on 6/4/13.
+//  Copyright (c) 2013 kumel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Erdioo/Erdioo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Erdioo
+//
+//  Created by Arie on 6/4/13.
+//  Copyright (c) 2013 kumel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
