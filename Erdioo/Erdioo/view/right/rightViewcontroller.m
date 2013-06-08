91bcb6736114d24ef926bb9ddd4e3f060c5778e7
Arie <arie@wego.com>
2013-06-08 18:23:11 +0700

    add rightviewcontroller

---
+++ Erdioo/Erdioo/view/right/rightViewcontroller.m
@@ -0,0 +1,38 @@
+//
+//  rightViewcontroller.m
+//  Erdioo
+//
+//  Created by Arie on 6/8/13.
+//  Copyright (c) 2013 kumel. All rights reserved.
+//
+
+#import "rightViewcontroller.h"
+
+@interface rightViewcontroller ()
+
+@end
+
+@implementation rightViewcontroller
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
