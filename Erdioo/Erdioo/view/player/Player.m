c479ad42d51eda44aebf6b9d0670763d20bd5820
Arie <arie@wego.com>
2013-06-08 13:09:42 +0700

    add player

---
+++ Erdioo/Erdioo/view/player/Player.m
@@ -0,0 +1,38 @@
+//
+//  Player.m
+//  Erdioo
+//
+//  Created by Arie on 6/8/13.
+//  Copyright (c) 2013 kumel. All rights reserved.
+//
+
+#import "Player.h"
+
+@interface Player ()
+
+@end
+
+@implementation Player
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
